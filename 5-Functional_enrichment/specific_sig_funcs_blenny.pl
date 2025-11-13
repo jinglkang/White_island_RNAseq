997ba7d42854dcb40d9be6f4d5c2c75fedfaad1d
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2025-11-13 11:28:26 +0800

    Create specific_sig_funcs_blenny.pl

---
+++ 5-Functional_enrichment/specific_sig_funcs_blenny.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my (%yal, %blu, %com);
+my $Yaldwyn="YaldwynNofilter_enrichment.txt";
+open YAL, $Yaldwyn or die "can not open $Yaldwyn\n";
+while (<YAL>) {
+	chomp;
+	next if /^Tags/;
+	my @a=split /\t/;
+	$yal{$a[2]}++ if $a[6]==0;
+}
+my $Blueyed="BlueeyedNofilter_enrichment.txt";
+open BLU, $Blueyed or die "can not open $Blueyed\n";
+while (<BLU>) {
+	chomp;
+	next if /^Tags/;
+	my @a=split /\t/;
+	$blu{$a[2]}++ if $a[6]==0;
+}
+
+my $common="CommonNofilter_enrichment.txt";
+open COM, $common or die "can not open $common\n";
+while (<COM>) {
+	chomp;
+	next if /^Tags/;
+	my @a=split /\t/;
+	$com{$a[2]}++ if $a[6]==0;
+}
+
+my $blenny="BlennyNofilter_enrichment.txt";
+open BLE, $blenny or die "can not open $blenny\n";
+while (<BLE>) {
+	chomp;
+	next if /^Tags/;
+	my @a=split /\t/;
+	print "$a[2]\n" if $yal{$a[2]} && $blu{$a[2]} && $com{$a[2]} && $a[4]<=0.05;
+}
